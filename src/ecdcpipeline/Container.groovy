1a25073c2b5cf62dc810c1edddf8704dde3b7ebb
Afonso Mukai <28659574+amues@users.noreply.github.com>
2018-09-17 15:11:46 +0200

    Add relative path copying capability to container

--- src/ecdcpipeline/Container.groovy
+++ src/ecdcpipeline/Container.groovy
@@ -21,10 +21,25 @@ class Container implements Serializable {
   }
 
   def copyTo(String src, String dst) {
-    return script.sh("docker cp ${src} ${name}:${dst}")
+    def resolvedPath = resolveContainerPath(dst)
+    return script.sh("docker cp ${src} ${name}:${resolvedPath}")
   }
 
   def copyFrom(String src, String dst) {
-    return script.sh("docker cp ${name}:${src} ${dst}")
+    def resolvedPath = resolveContainerPath(src)
+    return script.sh("docker cp ${name}:${resolvedPath} ${dst}")
+  }
+
+  private def resolveContainerPath(String path) {
+    String resolvedPath
+
+    def trimmedPath = path.trim()
+    if (trimmedPath.startsWith('/')) {
+      resolvedPath = trimmedPath
+    } else {
+      resolvedPath = '/home/jenkins/' + trimmedPath
+    }
+
+    return resolvedPath
   }
 }
