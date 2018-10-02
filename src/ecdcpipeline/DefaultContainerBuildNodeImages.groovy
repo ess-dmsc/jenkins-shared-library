ff6656788a9ba63de87800daaaab952b5a07daa6
Afonso Mukai <28659574+amues@users.noreply.github.com>
2018-10-02 14:08:10 +0200

    Bump default build node versions

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -4,15 +4,15 @@ package ecdcpipeline
 class DefaultContainerBuildNodeImages {
   static images = [
     'centos7': [
-      'image': 'essdmscdm/centos7-build-node:3.1.0',
+      'image': 'essdmscdm/centos7-build-node:3.2.0',
       'shell': '/usr/bin/scl enable devtoolset-6 -- /bin/bash -e -x'
     ],
     'debian9': [
-      'image': 'essdmscdm/debian9-build-node:2.1.0',
+      'image': 'essdmscdm/debian9-build-node:2.3.0',
       'shell': 'bash -e -x'
     ],
     'ubuntu1804': [
-      'image': 'essdmscdm/ubuntu18.04-build-node:1.1.0',
+      'image': 'essdmscdm/ubuntu18.04-build-node:1.2.0',
       'shell': 'bash -e -x'
     ]
   ]
