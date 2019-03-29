ebd7c819fc5d9be3390aa92064a182005c6ed0c6
Matthew D Jones <matthew-d-jones@users.noreply.github.com>
2019-03-29 12:14:50 +0000

    Update DefaultContainerBuildNodeImages.groovy

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -16,7 +16,7 @@ class DefaultContainerBuildNodeImages {
       'shell': 'bash -e -x'
     ],
     'alpine': [
-      'image': 'screamingudder/alpine-build-node:1.1.0',
+      'image': 'screamingudder/alpine-build-node:1.2.0',
       'shell': 'bash -e -x'
     ]
   ]
