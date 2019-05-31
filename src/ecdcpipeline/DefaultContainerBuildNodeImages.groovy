25987e7fabab2a29571f497baa2622c4b1975cfc
Matthew D Jones <matthew-d-jones@users.noreply.github.com>
2019-05-31 10:58:24 +0100

    Update DefaultContainerBuildNodeImages.groovy

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -4,19 +4,19 @@ package ecdcpipeline
 class DefaultContainerBuildNodeImages {
   static images = [
     'centos7': [
-      'image': 'screamingudder/centos7-build-node:4.5.0',
+      'image': 'screamingudder/centos7-build-node:4.6.0',
       'shell': '/usr/bin/scl enable devtoolset-6 -- /bin/bash -e -x'
     ],
     'debian9': [
-      'image': 'screamingudder/debian9-build-node:3.3.0',
+      'image': 'screamingudder/debian9-build-node:3.4.0',
       'shell': 'bash -e -x'
     ],
     'ubuntu1804': [
-      'image': 'screamingudder/ubuntu18.04-build-node:2.4.0',
+      'image': 'screamingudder/ubuntu18.04-build-node:2.5.0',
       'shell': 'bash -e -x'
     ],
     'alpine': [
-      'image': 'screamingudder/alpine-build-node:1.5.0',
+      'image': 'screamingudder/alpine-build-node:1.6.0',
       'shell': 'bash -e -x'
     ]
   ]
