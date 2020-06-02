c0b265193a5b0cd754052310648edc4735511665
Afonso Mukai <28659574+amues@users.noreply.github.com>
2020-06-02 14:14:24 +0200

    Bump Ubuntu build node version

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -24,7 +24,7 @@ class DefaultContainerBuildNodeImages {
       'shell': 'bash -e -x'
     ],
     'ubuntu1804-gcc8': [
-      'image': 'screamingudder/ubuntu18.04-build-node:3.0.6',
+      'image': 'screamingudder/ubuntu18.04-build-node:3.1.0',
       'shell': 'bash -e -x'
     ],
     'alpine': [
