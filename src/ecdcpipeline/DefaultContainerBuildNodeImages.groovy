8492236cc271ba9f3941973cf4ddfe19f5b6e396
Afonso Mukai <28659574+amues@users.noreply.github.com>
2021-04-22 23:56:35 +0200

    Bump build node versions

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -8,7 +8,7 @@ class DefaultContainerBuildNodeImages {
       'shell': '/usr/bin/scl enable devtoolset-6 -- /bin/bash -e -x'
     ],
     'centos7-gcc8': [
-      'image': 'screamingudder/centos7-build-node:5.6.0',
+      'image': 'screamingudder/centos7-build-node:6.0.0',
       'shell': '/usr/bin/scl enable devtoolset-8 -- /bin/bash -e -x'
     ],
     'debian9': [
@@ -16,7 +16,7 @@ class DefaultContainerBuildNodeImages {
       'shell': 'bash -e -x'
     ],
     'debian10': [
-      'image': 'screamingudder/debian10-build-node:1.2.0',
+      'image': 'screamingudder/debian10-build-node:4.0.0',
       'shell': 'bash -e -x'
     ],
     'ubuntu1804': [
@@ -24,11 +24,11 @@ class DefaultContainerBuildNodeImages {
       'shell': 'bash -e -x'
     ],
     'ubuntu1804-gcc8': [
-      'image': 'screamingudder/ubuntu18.04-build-node:3.3.0',
+      'image': 'screamingudder/ubuntu18.04-build-node:4.0.0',
       'shell': 'bash -e -x'
     ],
     'ubuntu2004': [
-      'image': 'screamingudder/ubuntu20.04-build-node:1.1.0',
+      'image': 'screamingudder/ubuntu20.04-build-node:2.0.0',
       'shell': 'bash -e -x'
     ]
   ]
