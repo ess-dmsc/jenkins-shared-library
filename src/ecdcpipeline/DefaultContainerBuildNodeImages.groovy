ea687fa48f268463165a769847394ed925f2efb0
Afonso Mukai <afonso.mukai@ess.eu>
2023-10-13 13:56:24 +0200

    Bump build node versions and update examples

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -4,23 +4,19 @@ package ecdcpipeline
 class DefaultContainerBuildNodeImages {
   static images = [
     'centos7-gcc11': [
-      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/centos7-build-node:11.2.0',
+      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/centos7-build-node:12.0.0',
       'shell': '/usr/bin/scl enable devtoolset-11 rh-python38 -- /bin/bash -e -x'
     ],
-    'centos7-gcc8': [
-      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/centos7-build-node:6.4.0',
-      'shell': '/usr/bin/scl enable devtoolset-8 rh-python38 -- /bin/bash -e -x'
-    ],
     'almalinux8-gcc12': [
-      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/almalinux8-build-node:0.2.0',
+      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/almalinux8-build-node:1.0.0',
       'shell': '/usr/bin/scl enable gcc-toolset-12 -- /bin/bash -e -x'
     ],
     'debian11': [
-      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/debian11-build-node:3.1.0',
+      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/debian11-build-node:4.0.0',
       'shell': 'bash -e -x'
     ],
     'ubuntu2204': [
-      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/ubuntu22.04-build-node:3.1.0',
+      'image': 'dockerregistry.esss.dk/ecdc_group/build-node-images/ubuntu22.04-build-node:4.0.0',
       'shell': 'bash -e -x'
     ]
   ]
