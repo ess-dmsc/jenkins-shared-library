4b5fe4f8bc123ac4de8b6ffa43962bfc1be33aa5
Mads Ipsen <mads.ipsen@gmail.com>
2025-02-14 09:48:01 +0100

    - Adding an entry for the ubuntu2204-qt6 build node

--- src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
+++ src/ecdcpipeline/DefaultContainerBuildNodeImages.groovy
@@ -7,18 +7,27 @@ class DefaultContainerBuildNodeImages {
       'image': 'registry.esss.lu.se/ecdc/ess-dmsc/build-nodes/centos7-qt6:1.0',
       'shell': '/usr/bin/scl enable devtoolset-11 rh-python38 -- /bin/bash -e -x'
     ],
+
     'centos7-gcc11': [
       'image': 'registry.esss.lu.se/ecdc/ess-dmsc/docker-centos7-build-node:12.3.0',
       'shell': '/usr/bin/scl enable devtoolset-11 rh-python38 -- /bin/bash -e -x'
     ],
+
     'almalinux8-gcc12': [
       'image': 'registry.esss.lu.se/ecdc/ess-dmsc/docker-almalinux8-build-node:1.1.0',
       'shell': '/usr/bin/scl enable gcc-toolset-12 -- /bin/bash -e -x'
     ],
+
     'debian11': [
       'image': 'registry.esss.lu.se/ecdc/ess-dmsc/docker-debian11-build-node:5.2.0',
       'shell': 'bash -e -x'
     ],
+
+    'ubuntu2204-qt6': [
+      'image': 'registry.esss.lu.se/ecdc/ess-dmsc/build-nodes/ubuntu2204-qt6:1.0',
+      'shell': 'bash -e -x'
+    ]
+
     'ubuntu2204': [
       'image': 'registry.esss.lu.se/ecdc/ess-dmsc/docker-ubuntu2204-build-node:5.0.0',
       'shell': 'bash -e -x'
